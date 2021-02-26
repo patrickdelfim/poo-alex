868b590141f51a3c82aeda3ab5aeaee6d5074000
Patrick Delfim Borges <patrickdelfim@gmail.com>
2021-02-25 21:41:36 -0300

    Tabuada

---
+++ src/tabuada.java
@@ -0,0 +1,16 @@
+
+public class tabuada {
+
+	public static void main(String[] args) {
+		/* Patrick Delfim Borges	
+		 *  0050016717          */
+		
+		for(int i=0; i<=10;i++) {
+			
+			System.out.println(10 * i);
+		}
+		
+
+	}
+
+}
